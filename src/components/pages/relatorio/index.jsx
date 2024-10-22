b1c85e0433f930931a850817835074af9d7e3b45
controltowersp10 <controltowersp10@gmail.com>
2024-10-22 20:57:53 +0000

    teste copia

--- src/components/pages/relatorio/index.jsx
+++ src/components/pages/relatorio/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+//teste copia 
+import React, { useState, useEffect } from "react"
 import { Helmet } from "react-helmet";
 import app from "../../../firebaseConfig.jsx";
 import { getDatabase, ref, get, set } from "firebase/database";
